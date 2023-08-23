a5caaf137db80742b2b348cde5ebadfdd45b5e72
Tomasz <pawlowski.t.93@gmail.com>
2023-08-23 17:25:43 +0200

    Fixed task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,10 @@
 const textInput = document.querySelector("#validation-input");
 
 textInput.addEventListener("blur", () => {
-  if (textInput.value.length < textInput.getAttribute("data-length")) {
+  if (
+    textInput.value.length < textInput.getAttribute("data-length") ||
+    textInput.value.length > textInput.getAttribute("data-length")
+  ) {
     textInput.classList.add("invalid");
   } else {
     textInput.classList.remove("invalid");
